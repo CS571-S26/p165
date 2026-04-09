67e67939b8382eda2970f30115d61f4c9f5ecb78
Ryan Miller <ryan@Ryans-Air-2.lan>
2026-04-08 20:05:25 -0500

    more router updates

--- src/components/TheRouterTM.jsx
+++ src/components/TheRouterTM.jsx
@@ -1,4 +1,9 @@
 import { HashRouter, Route, Routes } from "react-router";
+import HomePage from "./pages/HomePage";
+import ListsPage from "./pages/ListsPage";
+import BudgetPage from "./pages/BudgetPage";
+import RoutinesPage from "./pages/RoutinesPage";
+import AboutPage from "./pages/AboutPage";
 
 export default function TheRouterTM()
 {
@@ -8,6 +13,7 @@ export default function TheRouterTM()
                 <Route path="/lists" element={<ListsPage />} />
                 <Route path="/routines" element={<RoutinesPage />} />
                 <Route path="/budget" element={<BudgetPage />} />
+                <Route path="/about" element={<AboutPage />} />
             </Route>
         </Routes>
     </HashRouter>
