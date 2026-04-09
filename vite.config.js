202c1e7e2c2d194d0b5a3a244fed0fefeed6bc86
Ryan Miller <ryan@Ryans-Air-2.lan>
2026-04-08 20:26:23 -0500

    Fixed config base issue

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/p165',
+  base: '/p165/',
   plugins: [react()],
   build: {
     outDir: "docs"
