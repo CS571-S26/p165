295e60afe4c87dc6a05b68ee08b1cde06eead888
Ryan Miller <ryan@Ryans-Air-2.lan>
2026-04-08 20:20:10 -0500

    Deleted app and stuff from default build

--- src/components/TheRouterTM.jsx
+++ src/components/TheRouterTM.jsx
@@ -9,7 +9,8 @@ export default function TheRouterTM()
 {
     return <HashRouter>
         <Routes>
-            <Route path="/" element={<HomePage />}>
+            <Route path="/" element={<Layout />}>
+                <Route path="/home" index element={<HomePage />} />
                 <Route path="/lists" element={<ListsPage />} />
                 <Route path="/routines" element={<RoutinesPage />} />
                 <Route path="/budget" element={<BudgetPage />} />
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import App from './App.jsx'
 import { HashRouter } from 'react-router-dom'
 import TheRouterTM from './components/TheRouterTM.jsx'
 import ReactDOM from 'react-dom/client';
