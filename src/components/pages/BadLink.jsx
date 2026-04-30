7c3917a3fc02b717af0f5cde28e5f1edc07b7a15
Ryan Miller <ryan@Ryans-Air-2.lan>
2026-04-29 22:11:19 -0500

    Will be added next build, forgot to commit it

--- src/components/pages/BadLink.jsx
+++ src/components/pages/BadLink.jsx
@@ -2,12 +2,14 @@ import { Link } from "react-router";
 
 export default function BadLink() {
     return (
+        <div className="page-background">
         <div className="page-layout">
             <h2>That's a 404.</h2>
-            <p>Uh oh, looks like you've taken a wrong turn!</p>
+            <p>Uh-oh! Someone has ventured out into the Void! It'll get totally SPOILED!</p>
             <p>
                 <Link to="/">Back to safety.</Link>
             </p>
+        </div>
         </div>
     );
 }
